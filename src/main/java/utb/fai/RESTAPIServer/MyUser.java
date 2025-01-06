ff43f33a847f99602ddb4e43721a6c707c3bef1d
RadekSusen <95688472+RadekSusen@users.noreply.github.com>
2025-01-06 01:54:28 +0100

    Update MyUser.java

--- src/main/java/utb/fai/RESTAPIServer/MyUser.java
+++ src/main/java/utb/fai/RESTAPIServer/MyUser.java
@@ -1,14 +1,25 @@
 package utb.fai.RESTAPIServer;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+import java.util.jar.Attributes.Name;
+import java.util.regex.Pattern;
+
+@Entity
 public class MyUser {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phoneNumber;
 
-    public MyUser() {}
+    public MyUser() {
+    }
 
     public MyUser(String name, String email, String phoneNumber) {
         this.name = name;
@@ -19,9 +30,57 @@ public class MyUser {
     public boolean isUserDataValid() {
         // Add your validation logic here (e.g., email and phone number format
         // validation)
-        return true;
+        return validMail(this.email) && validPhone(this.phoneNumber) && validName(this.name);
+    }
+
+    private boolean validMail(String email) {
+        String emailReg = "^[A-Za-z0-9\\\\+_\\\\.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(emailReg);
+        return pattern.matcher(email).matches();
+    }
+
+    private boolean validPhone(String phoneNumber) {
+        String phoneReg = "^\\+[0-9]*$";
+        Pattern pattern = Pattern.compile(phoneReg);
+        return pattern.matcher(phoneNumber).matches();
+    }
+
+    private boolean validName(String name) {
+        return name.length() > 3;
     }
 
     // TODO: Getters and setters
-    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
 }
