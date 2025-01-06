7710b82e0d2ae971b6fa7563433eac039e003814
RadekSusen <95688472+RadekSusen@users.noreply.github.com>
2025-01-06 01:54:48 +0100

    Update UserController.java

--- src/main/java/utb/fai/RESTAPIServer/UserController.java
+++ src/main/java/utb/fai/RESTAPIServer/UserController.java
@@ -1,7 +1,9 @@
 package utb.fai.RESTAPIServer;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,28 +11,74 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/users")
     public List<MyUser> getAllUsers() {
         // TODO: implement logic of /users endpoint
-        return null;
+
+        return userRepository.findAll();
     }
 
     @GetMapping("/getUser")
     public ResponseEntity<MyUser> getUserById(@RequestParam Long id) {
         // TODO: implement logic of /getUser endpoint
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        Optional<MyUser> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/createUser")
     public ResponseEntity<MyUser> createUser(@RequestBody MyUser newUser) {
         // TODO: implement logic of /createUser endpoint
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (newUser.isUserDataValid()) {
+            return new ResponseEntity<>(userRepository.save(newUser), HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
-    // TODO: Define remaining endpoints in the same way. For id parameter use annotation @RequestParam with name "id" and for MyUser structure use @RequestBody.
+    // TODO: Define remaining endpoints in the same way. For id parameter use
+    // annotation @RequestParam with name "id" and for MyUser structure use
+    // @RequestBody.
 
+    @PutMapping("/editUser")
+    public ResponseEntity<MyUser> updateUser(@RequestParam("id") Long id, @RequestBody MyUser userData) {
+        if (!userData.isUserDataValid()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return userRepository.findById(id).map(user -> {
+            user.setEmail(userData.getEmail());
+            user.setName(userData.getName());
+            user.setPhoneNumber(userData.getPhoneNumber());
+
+            return ResponseEntity.ok(userRepository.save(user));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/deleteUser")
+    public ResponseEntity<Void> deleteUser(@RequestParam("id") Long id) {
+        return userRepository.findById(id).map(user -> {
+            userRepository.delete(user);
+            return ResponseEntity.ok().<Void>build();
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/deleteAll")
+    public ResponseEntity<Void> deleteAllUser() {
+        userRepository.deleteAll();
+        return ResponseEntity.ok().<Void>build();
+    }
 }
